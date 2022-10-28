74fbee1a9cd874819acab4868b1b64249f716b5f
Gideon Opoku <opokugideon562@gmail.com>
2022-10-28 22:56:07 +0000

    Update simpleEtherFaucet.sol

--- simpleEtherFaucet.sol
+++ simpleEtherFaucet.sol
@@ -1,6 +1,4 @@
-
 //A faucet is a relatively simple thing: it gives out ether to any address that asks, and can be refilled periodically
-
 
 pragma solidity ^0.4.19;
 
@@ -8,12 +6,14 @@ contract Faucet{
 
     //Give out ether to anyone who asks
     function withdraw(uint withdrawAmount) public{
+    
         //Limit withdrawal amount
         require(withdrawAmount <= 1000000000000000000);
 
         //Send the amount to the address that requested it
         msg.sender.transfer(withdrawAmount);
     }
+    
     //Accept any incoming amount
     function () public payable{}
 }
